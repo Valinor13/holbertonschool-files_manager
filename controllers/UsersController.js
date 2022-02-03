a209c6b0dd73b3f92b16ac5272aa2e8a3962dfb5
Valinor13 <jwcalhoun2@gmail.com>
2022-02-03 17:40:31 -0600

    Adding users controller

---
+++ controllers/UsersController.js
@@ -0,0 +1,28 @@
+const sha1 = require('sha1');
+const dbClient = require('../utils/db');
+
+const db = dbClient.db.collection('users');
+
+class UsersController {
+  static postUser(req, res) {
+    (async () => {
+      if (!req.params.email) {
+        res.status(400).send('Missing email');
+      }
+      if (!req.params.password) {
+        res.status(400).send('Missing password');
+      }
+      if (await db.findOne({ email: req.params.email })) {
+        res.status(400).send('Already exist');
+      } else {
+        const hashPw = sha1(req.params.password);
+        const doc = { email: req.params.email, password: hashPw };
+        const result = await db.insertOne(doc);
+        res.status(201).send(JSON.stringify({ id: result.insertedId, email: req.params.email }));
+      }
+      res.end();
+    })();
+  }
+}
+
+module.exports = UsersController;
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const index = require('./routes/index');
+const router = require('./routes/index');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(index);
+app.use(router);
 
 app.listen(port, () => {
   console.log(`server is running at ${port}`);
