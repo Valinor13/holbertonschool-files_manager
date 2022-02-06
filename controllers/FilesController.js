49571f29dc32ef2154027f32eaefaaeef3cca349
Rachel Lewis <RLewis11769@gmail.com>
2022-02-06 13:08:49 -0600

    Add publish and unpublish to FilesController

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -129,6 +129,54 @@ class FilesController {
       return res.status(404).json({ error: 'Not found' });
     })();
   }
+
+  static putPublish(req, res) {
+    (async () => {
+      const header = req.headers['x-token'];
+      const token = `auth_${header}`;
+      const user = await Redis.get(token);
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      const userId = new ObjectID(user);
+      const fileId = new ObjectID(req.params.id);
+      const file = await files.findOne({ _id: fileId, userId });
+      if (!file) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+      await files.updateOne({ _id: fileId }, { $set: { isPublic: true } });
+      file.id = file._id.toString();
+      file.isPublic = true;
+      file.userId.toString();
+      file.parentId.toString();
+      delete file._id;
+      return res.status(200).json(file);
+    })();
+  }
+
+  static putUnpublish(req, res) {
+    (async () => {
+      const header = req.headers['x-token'];
+      const token = `auth_${header}`;
+      const user = await Redis.get(token);
+      if (!user) {
+        return res.status(401).json({ error: 'Unauthorized' });
+      }
+      const userId = new ObjectID(user);
+      const fileId = new ObjectID(req.params.id);
+      const file = await files.findOne({ _id: fileId, userId });
+      if (!file) {
+        return res.status(404).json({ error: 'Not found' });
+      }
+      await files.updateOne({ _id: fileId }, { $set: { isPublic: false } });
+      file.id = file._id.toString();
+      file.isPublic = false;
+      file.userId.toString();
+      file.parentId.toString();
+      delete file._id;
+      return res.status(200).json(file);
+    })();
+  }
 }
 
 module.exports = FilesController;
